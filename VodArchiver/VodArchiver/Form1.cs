d14b857d29b06e977c36660209dc32936d6faec0
Admiral H. Curtiss <pikachu025@gmail.com>
2016-02-07 21:27:26 +0100

    Functionality to download the video files from the M3U8, combine them, and remux them.
    A bit hacked together but it'll do for now.

--- VodArchiver/VodArchiver/Form1.cs
+++ VodArchiver/VodArchiver/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,16 +19,78 @@ namespace VodArchiver {
 		}
 
 		private async void button1_Click( object sender, EventArgs e ) {
-			Task<string[]> task = GetFileUrlsOfTwitchVod( twixel, "v16680710" );
+			string id = "v16680710";
+			Task<string[]> task = GetFileUrlsOfTwitchVod( twixel, id );
 			await task;
 
 			if ( task.Status == TaskStatus.RanToCompletion ) {
-				foreach ( string s in task.Result ) {
-					Console.WriteLine( s );
-				}
+				DownloadAndCombineFilePartsTS( id + "-twitch", task.Result );
 			} else {
 				Console.WriteLine( "error!" );
 			}
+		}
+
+		public static void DownloadAndCombineFilePartsTS( string targetFolder, string[] urls ) {
+			Directory.CreateDirectory( targetFolder );
+
+			// download files
+			List<string> files = new List<string>( urls.Length );
+			for ( int i = 0; i < urls.Length; ++i ) {
+				string url = urls[i];
+				string outpath = Path.Combine( targetFolder, "part" + i.ToString( "D8" ) + ".ts" );
+				string outpath_temp = outpath + ".tmp";
+				if ( File.Exists( outpath_temp ) ) {
+					File.Delete( outpath_temp );
+				}
+				if ( File.Exists( outpath ) ) {
+					Console.WriteLine( "Already have " + url + "..." );
+					files.Add( outpath );
+					continue;
+				}
+
+				using ( var client = new System.Net.WebClient() ) {
+					bool success = false;
+					while ( !success ) {
+						try {
+							Console.WriteLine( "Downloading " + url + "..." );
+							client.DownloadFile( url, outpath_temp );
+							success = true;
+						} catch ( System.Net.WebException ex ) {
+							Console.WriteLine( ex.ToString() );
+							continue;
+						}
+					}
+				}
+
+				File.Move( outpath_temp, outpath );
+				files.Add( outpath );
+			}
+
+			// combine
+			string combinedFilename = Path.Combine( targetFolder, "combined.ts" );
+			Console.WriteLine( "Combining into " + combinedFilename + "..." );
+			using ( var fs = new FileStream( combinedFilename, FileMode.Create ) ) {
+				foreach ( var file in files ) {
+					using ( var part = new FileStream( file, FileMode.Open ) ) {
+						part.CopyTo( fs );
+					}
+				}
+
+				fs.Close();
+			}
+
+			// remux
+			string finalFilename = Path.Combine( targetFolder, "final.mp4" );
+			Console.WriteLine( "Remuxing to " + finalFilename + "..." );
+			var inputFile = new MediaToolkit.Model.MediaFile( combinedFilename );
+			var outputFile = new MediaToolkit.Model.MediaFile( finalFilename );
+			using ( var engine = new MediaToolkit.Engine( "ffmpeg.exe" ) ) {
+				MediaToolkit.Options.ConversionOptions options = new MediaToolkit.Options.ConversionOptions();
+				options.AdditionalOptions = "-codec copy -bsf:a aac_adtstoasc";
+				engine.Convert( inputFile, outputFile, options );
+			}
+
+			Console.WriteLine( "Created " + finalFilename + "!" );
 		}
 
 		public static string GetM3U8PathFromM3U_Twitch( string m3u, string videoType ) {
