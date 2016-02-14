bfd2634e02e94b8bc7f4f0a6fd4ed917ef6e6741
Admiral H. Curtiss <pikachu025@gmail.com>
2016-02-14 23:15:57 +0100

    Allow pressing Enter to start download

--- VodArchiver/VodArchiver/Form1.Designer.cs
+++ VodArchiver/VodArchiver/Form1.Designer.cs
@@ -185,6 +185,7 @@
 			// 
 			// Form1
 			// 
+			this.AcceptButton = this.buttonDownload;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(945, 477);
