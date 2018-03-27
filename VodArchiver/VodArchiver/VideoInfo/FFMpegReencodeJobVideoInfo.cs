173fb1e64940275937dc338affd48fd077c8327b
Admiral H. Curtiss <pikachu025@gmail.com>
2018-03-27 22:34:49 +0200

    Show the new postfix in the username column for reencoding tasks.

--- VodArchiver/VodArchiver/VideoInfo/FFMpegReencodeJobVideoInfo.cs
+++ VodArchiver/VodArchiver/VideoInfo/FFMpegReencodeJobVideoInfo.cs
@@ -10,14 +10,14 @@ namespace VodArchiver.VideoInfo {
 		public override StreamService Service { get => StreamService.FFMpegJob; set => throw new Exception(); }
 		public override RecordingState VideoRecordingState { get => RecordingState.Recorded; set => throw new Exception(); }
 		public override VideoFileType VideoType { get => VideoFileType.Unknown; set => throw new Exception(); }
-		public override string VideoTitle { get => Username; set => throw new Exception(); }
+		public override string Username { get => PostfixNew; set => throw new Exception(); }
 
 		public List<string> FFMpegOptions;
 		public string PostfixOld;
 		public string PostfixNew;
 
 		public FFMpegReencodeJobVideoInfo( GenericVideoInfo videoInfo, List<string> ffmpegOptions, string postfixOld, string postfixNew ) {
-			Username = videoInfo.Username;
+			VideoTitle = videoInfo.Username;
 			VideoId = videoInfo.VideoId;
 			VideoGame = videoInfo.VideoGame;
 			VideoTimestamp = videoInfo.VideoTimestamp;
@@ -28,7 +28,7 @@ namespace VodArchiver.VideoInfo {
 		}
 
 		public FFMpegReencodeJobVideoInfo( XmlNode node ) {
-			Username = node.Attributes["username"].Value;
+			VideoTitle = node.Attributes["username"].Value;
 			VideoId = node.Attributes["videoId"].Value;
 			VideoGame = node.Attributes["videoTags"].Value;
 			VideoTimestamp = DateTime.FromBinary( long.Parse( node.Attributes["videoTimestamp"].Value ) );
@@ -49,7 +49,7 @@ namespace VodArchiver.VideoInfo {
 
 		public override XmlNode Serialize( XmlDocument document, XmlNode node ) {
 			node.AppendAttribute( document, "_type", "FFMpegReencodeJobVideoInfo" );
-			node.AppendAttribute( document, "username", Username );
+			node.AppendAttribute( document, "username", VideoTitle );
 			node.AppendAttribute( document, "videoId", VideoId );
 			node.AppendAttribute( document, "videoTags", VideoGame );
 			node.AppendAttribute( document, "videoTimestamp", VideoTimestamp.ToBinary().ToString() );
