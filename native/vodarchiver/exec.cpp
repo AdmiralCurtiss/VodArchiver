c7cf21d65aae85bc3d34b3bc12ee494aec2bc969
Admiral H. Curtiss <pikachu025@gmail.com>
2025-08-10 15:44:54 +0200

    Use WSTR explicitly in CreateProcessWithExplicitHandles()

--- native/vodarchiver/exec.cpp
+++ native/vodarchiver/exec.cpp
@@ -91,19 +91,20 @@ std::string BuildArgsString(const std::string& programName, const std::vector<st
 }
 
 // from https://devblogs.microsoft.com/oldnewthing/20111216-00/?p=8873
-BOOL CreateProcessWithExplicitHandles(__in_opt LPCTSTR lpApplicationName,
-                                      __inout_opt LPTSTR lpCommandLine,
-                                      __in_opt LPSECURITY_ATTRIBUTES lpProcessAttributes,
-                                      __in_opt LPSECURITY_ATTRIBUTES lpThreadAttributes,
-                                      __in BOOL bInheritHandles,
-                                      __in DWORD dwCreationFlags,
-                                      __in_opt LPVOID lpEnvironment,
-                                      __in_opt LPCTSTR lpCurrentDirectory,
-                                      __in LPSTARTUPINFO lpStartupInfo,
-                                      __out LPPROCESS_INFORMATION lpProcessInformation,
-                                      // here is the new stuff
-                                      __in DWORD cHandlesToInherit,
-                                      __in_ecount(cHandlesToInherit) HANDLE* rgHandlesToInherit) {
+static BOOL CreateProcessWithExplicitHandles(__in_opt LPCWSTR lpApplicationName,
+                                             __inout_opt LPWSTR lpCommandLine,
+                                             __in_opt LPSECURITY_ATTRIBUTES lpProcessAttributes,
+                                             __in_opt LPSECURITY_ATTRIBUTES lpThreadAttributes,
+                                             __in BOOL bInheritHandles,
+                                             __in DWORD dwCreationFlags,
+                                             __in_opt LPVOID lpEnvironment,
+                                             __in_opt LPCWSTR lpCurrentDirectory,
+                                             __in LPSTARTUPINFO lpStartupInfo,
+                                             __out LPPROCESS_INFORMATION lpProcessInformation,
+                                             // here is the new stuff
+                                             __in DWORD cHandlesToInherit,
+                                             __in_ecount(cHandlesToInherit)
+                                                 HANDLE* rgHandlesToInherit) {
     BOOL fSuccess;
     BOOL fInitialized = FALSE;
     SIZE_T size = 0;
@@ -136,21 +137,20 @@ BOOL CreateProcessWithExplicitHandles(__in_opt LPCTSTR lpApplicationName,
                                              NULL);
     }
     if (fSuccess) {
-        STARTUPINFOEX info;
-        ZeroMemory(&info, sizeof(info));
+        STARTUPINFOEX info{};
         info.StartupInfo = *lpStartupInfo;
         info.StartupInfo.cb = sizeof(info);
         info.lpAttributeList = lpAttributeList;
-        fSuccess = CreateProcess(lpApplicationName,
-                                 lpCommandLine,
-                                 lpProcessAttributes,
-                                 lpThreadAttributes,
-                                 bInheritHandles,
-                                 dwCreationFlags | EXTENDED_STARTUPINFO_PRESENT,
-                                 lpEnvironment,
-                                 lpCurrentDirectory,
-                                 &info.StartupInfo,
-                                 lpProcessInformation);
+        fSuccess = CreateProcessW(lpApplicationName,
+                                  lpCommandLine,
+                                  lpProcessAttributes,
+                                  lpThreadAttributes,
+                                  bInheritHandles,
+                                  dwCreationFlags | EXTENDED_STARTUPINFO_PRESENT,
+                                  lpEnvironment,
+                                  lpCurrentDirectory,
+                                  &info.StartupInfo,
+                                  lpProcessInformation);
     }
     if (fInitialized)
         DeleteProcThreadAttributeList(lpAttributeList);
